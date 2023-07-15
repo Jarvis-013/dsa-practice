43c98bd5aa582b83035bd077ac422ced679c607e
Jarvis-013 <rajatbhaskar001@gmail.com>
2023-07-16 00:00:12 +0530

    changes

--- sliding_window/housing.cpp
+++ sliding_window/housing.cpp
@@ -4,4 +4,27 @@
 #include <algorithm>
 using namespace std;
 
-vector<vector<int>> housing
+void housing(vector<int> &arr,int n,int k){
+    int i=0;
+    int j=0,sum=0;
+    while (\j<n)
+    {
+    sum+=arr[j]; 
+    j++;
+    while (sum> k && i<j)
+    {
+        cs=cs-arr[i];
+        i++;
+    }
+    if(cs==k){
+        cout<<i<< " - "<<j-1<<endl;
+    }
+}
+return;
+}
+int main(){
+    int plot[]={1,2,3,4,5,6,7,8};
+    int n=sizeof(plots)/sizeof(int);
+    int k=8;
+    housing(plots,n,k);
+}
